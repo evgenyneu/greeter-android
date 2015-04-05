0112507a8ff8ee8119ac39b13b3dd875339ac02b
Evgenii Neumerzhitckii <evgeny@bikeexchange.com.au>
2015-04-05 17:45:52 +1000

    Construct greeting message

--- app/src/androidTest/java/com/mycompany/greeter/MainActivityTests.java
+++ app/src/androidTest/java/com/mycompany/greeter/MainActivityTests.java
@@ -1,5 +1,9 @@
 package com.mycompany.greeter;
 import android.test.ActivityInstrumentationTestCase2;
+import android.test.TouchUtils;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivityTests extends ActivityInstrumentationTestCase2<MainActivity> {
     public MainActivityTests() {
@@ -9,5 +13,39 @@ public class MainActivityTests extends ActivityInstrumentationTestCase2<MainActi
     public void testActivityExists() {
         MainActivity activity = getActivity();
         assertNotNull(activity);
+    }
+
+    public void testGreet() {
+        MainActivity activity = getActivity();
+
+        // Type name in text input
+        // ----------------------
+
+        final EditText nameEditText = (EditText) activity.findViewById(R.id.greet_edit_text);
+
+        // Send string input value
+        getInstrumentation().runOnMainSync(new Runnable() {
+            @Override
+            public void run() {
+                nameEditText.requestFocus();
+            }
+        });
+
+        getInstrumentation().waitForIdleSync();
+        getInstrumentation().sendStringSync("Jake");
+        getInstrumentation().waitForIdleSync();
+
+        // Tap "Greet" button
+        // ----------------------
+
+        Button greetButton = (Button) activity.findViewById(R.id.greet_button);
+        TouchUtils.clickView(this, greetButton);
+
+        // Verify greet message
+        // ----------------------
+
+        TextView greetMessage = (TextView) activity.findViewById(R.id.message_text_view);
+        String actualText = greetMessage.getText().toString();
+        assertEquals("Hello, Jake!", actualText);
     }
 }
--- app/src/main/java/com/mycompany/greeter/MainActivity.java
+++ app/src/main/java/com/mycompany/greeter/MainActivity.java
@@ -4,6 +4,9 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -35,5 +38,14 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void didTapGreetButton(View view) {
+        EditText greetEditText = (EditText) findViewById(R.id.greet_edit_text);
+        String name = greetEditText.getText().toString();
+        String greeting = String.format("Hello, %s!", name);
+
+        TextView messageTextView = (TextView) findViewById(R.id.message_text_view);
+        messageTextView.setText(greeting);
     }
 }
